903b8de1a78567e7f2c517983d4866da857c5f37
jakobsym <jakob.symchych@my.utsa.edu>
2023-01-13 12:43:56 -0600

    implement feedforward into network

--- neuron.py
+++ neuron.py
@@ -9,6 +9,16 @@ Neuron: Takes inputs, does (math) with them and produces one output
 Feedforward: Process of passing inputs forward to get an output
 """
 
+
+"""
+Neural Network put simply:
+	- Is a Network of layers, w/ any number of neurons (input layer, neuron(hidden) layer, output layer)
+	- Feed input(s) forward through neurons to get output(s)
+"""
+
+
+
+
 import numpy as np
 
 
@@ -26,12 +36,46 @@ class Neuron:
 		total = np.dot(self.weight, input) + self.bias
 		return sigmoid(total)
 
+class NeuralNetwork:
+
+	"""
+	- 2 input
+	- hidden layer w/ 2 neurons(h1, h2)
+	- output layer w/ 1 neuron (o1)
+
+	weight = [0,1]
+	bias = 0
+	"""
 
 
-weight = np.array([0,1])
-bias = 4
-n = Neuron(weight, bias)
+	def __init__(self):
+		weights = np.array([0,1])
+		bias = 0
+
+
+		self.h1 = Neuron(weights, bias)
+		self.h2 = Neuron(weights, bias)
+
+		self.o1 = Neuron(weights, bias)
+
+	def feedforward(self, x):
+		h1_out = self.h1.feedforward(x)
+		h2_out = self.h2.feedforward(x)
+
+		o1_out = self.o1.feedforward(np.array([h1_out, h2_out]))
+
+		return o1_out
+
+
+network = NeuralNetwork()
 
 x = np.array([2,3])
+print(network.feedforward(x))
 
-print(n.feedforward(x))
+
+
+
+
+
+
+
